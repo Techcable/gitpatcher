5acaec8ca5416eb7fed45a72fade0a16e764ab46
Techcable <Techcable@techcable.net>
2020-04-29 16:18:20 -0700

    Initial (untested) email parsing
    Upstream still hasn't accepted my PRs for git2-rs......

---
+++ src/apply_patches.rs
@@ -0,0 +1,138 @@
+use chrono::{Utc, DateTime, FixedOffset};
+use git2::{Oid, Diff, Repository};
+use regex::{Regex, Captures};
+use std::str::Lines;
+use lazy_static::lazy_static;
+use itertools::PeekingNext;
+
+struct EmailMessage {
+     upstream_commit: Oid,
+     date: DateTime<FixedOffset>,
+     message_summary: String,
+     message_tail: String,
+     author_name: String,
+     author_email: String,
+     diff: Diff<'static>
+}
+lazy_static! {
+     static ref HEADER_LINE: Regex = Regex::new("^From ([0-9A-F]{1,40}) Mon Sep 17 00:00:00 2001$").unwrap();
+     static ref AUTHOR_LINE: Regex = Regex::new("^From (.*) <.*>$").unwrap();
+     static ref DATE_LINE: Regex = Regex::new(r#"^Date (.* [\+-]\d+)$"#).unwrap();
+     static ref SUBJECT_LINE: Regex = Regex::new(r#"^Subject: [Patch[^\]]*] (.*)$"#).unwrap();
+     static ref BEGIN_DIFF_LINE: Regex = Regex::new(r"#^diff --git a/(.*) b/(.*)$")
+}
+fn match_header_line<'a>(
+     lines: &mut dyn Iterator<Item=&'a str>,
+     expected: &'static str,
+     pattern: &Regex
+) -> Result<Captures<'a>, InvalidEmailMessage> {
+     let line = lines.next()
+         .ok_or(InvalidEmailMessage::UnexpectedEof { expected })?;
+     pattern.captures(line).ok_or_else(|| InvalidEmailMessage::InvalidHeader {
+          expected, actual: line.into()
+     })
+}
+impl EmailMessage {
+     pub fn parse(msg: &str) -> Result<Self, InvalidEmailMessage> {
+          let diff = Diff::from_buffer(msg.as_bytes())?;
+
+          let mut lines = msg.lines().peekable();
+          let header = match_header_line(
+               &mut lines,
+               "header",
+               &*HEADER_LINE
+          )?;
+          let author = match_header_line(
+               &mut lines,
+               "author",
+               &*AUTHOR_LINE
+          )?;
+          let date = match_header_line(
+               &mut lines,
+               "date",
+               &*DATE_LINE
+          )?;
+          let subject = match_header_line(
+               &mut lines,
+               "subject",
+               &*SUBJECT_LINE
+          )?;
+          let mut message_subject = String::from(&subject[1]);
+          loop {
+               let line = lines.next().ok_or(InvalidEmailMessage::UnexpectedEof {
+                    expected: "diff after subject"
+               })?;
+               if line.is_empty() {
+                    break
+               } else {
+                    // I consider breaking over newlines an implicit space
+                    message_subject.push(' ');
+                    message_subject.push_str(line);
+               }
+          }
+          /*
+           * We already skipped a single line of whitespace
+           * There could be several lines of `message_tail`,
+           * than a single line,
+           * than `git diff -a/{some_file} b/{some_file}`
+           */
+          let mut trailing_message = String::new();
+          loop {
+               let line = lines.next().ok_or(InvalidEmailMessage::UnexpectedEof {
+                    expected: "diff after message"
+               })?;
+               if line.is_empty() {
+                    match lines.peek() {
+                         Some(line) if BEGIN_DIFF_LINE.is_match(line) => break,
+                         _ => {
+                              trailing_message.push('\n');
+                              // NOTE: None is implicitly handled by error in next iteration
+                              continue
+                         },
+
+                    }
+               } else {
+                    trailing_message.push_str(line);
+                    trailing_message.push('\n');
+               }
+          }
+          if trailing_message.ends_with('\n') {
+               assert_eq!(trailing_message.pop(), Some('\n'));
+          }
+          let author_name = &author[1];
+          let author_email = &author[2];
+          let date = DateTime::parse_from_rfc2822(&date[1])
+              .map_err(|cause| {
+                   InvalidEmailMessage::InvalidDate { cause, actual: date[1].into() }
+              })?;
+          let upstream_commit = Oid::from_str(&header[1]).unwrap();
+          Ok(EmailMessage {
+               diff, upstream_commit, date,
+               message_summary, message_tail,
+               author_name: author_name.into(),
+               author_email: author_email.into_string()
+          })
+
+     }
+}
+
+#[derive(Debug)]
+pub enum InvalidEmailMessage {
+     UnexpectedEof {
+          expected: &'static str
+     },
+     InvalidHeader {
+          expected: &'static str,
+          actual: String
+     },
+     InvalidDate {
+          actual: String,
+          cause: chrono::ParseError
+     },
+     Git(git2::Error)
+}
+impl From<git2::Error> for InvalidEmailMessage {
+     fn from(cause: git2::Error) -> Self {
+          InvalidEmailMessage::Git(cause)
+     }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
 pub mod regenerate_patches;
 pub mod format_patches;
+pub mod apply_patches;
 mod utils;

4ebf523df885176afd068354b02a33e94f7043c9
Techcable <git@techcable.net>
2026-06-26 21:37:59 -0700

    Sync test-paper-patch README using cargo-reedme
    Was not caught until now because of the missing --workspace flag.

--- crates/test-paper-patch/src/main.rs
+++ crates/test-paper-patch/src/main.rs
@@ -1,9 +1,18 @@
 #!/usr/bin/env rust-script --debug
-//! Tests gitpatcher by applying & rebuilding patches
+//! An *experimental* test for gitpatcher by applying & rebuilding patches
 //! for the [Paper](https://papermc.io/) minecraft project.
 //!
+//! It is for internal use only, not for consumption by users.
 //! Paper normally uses the [paperweight](https://github.com/PaperMC/paperweight)
 //! plugin for the [gradle](https://gradle.org/) build tool.
+//!
+//! The Paper patch system is the inspiration for this project.
+//! Since Paper includes hundreds of patches, it makes an excellent test case.
+//!
+//! This crate is useful for fixing the [excessive churn issue (#1)][excessive-churn-issue],
+//! as the PaperMC patch system is already able to filter it out.q
+//!
+//! [excessive-churn-issue]: https://github.com/Techcable/gitpatcher/issues/1
 use std::ffi::OsString;
 use std::path::{Path, PathBuf};
 use std::sync::OnceLock;
